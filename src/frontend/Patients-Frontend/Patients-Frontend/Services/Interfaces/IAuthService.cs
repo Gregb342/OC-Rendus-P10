19ceac716fe7cca33fbdb793900378d1f132d37f
Gregoire Bouteillier <gregoire.bouteillier@gmail.com>
2025-10-29 13:53:31 +0100

    Adding Services Interfaces

---
+++ src/frontend/Patients-Frontend/Patients-Frontend/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using Patients_Frontend.DTOs;
+
+namespace Patients_Frontend.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<bool> LoginAsync(LoginDto loginDto);
+        Task LogoutAsync();
+        Task<bool> IsAuthenticatedAsync();
+        Task<string?> GetTokenAsync();
+    }
+}
---
+++ src/frontend/Patients-Frontend/Patients-Frontend/Services/Interfaces/IPatientService.cs
@@ -0,0 +1,13 @@
+﻿using Patients_Frontend.DTOs;
+
+namespace Patients_Frontend.Services.Interfaces
+{
+    public interface IPatientService
+    {
+        Task<List<PatientDto>> GetAllPatientsAsync();
+        Task<PatientDto?> GetPatientByIdAsync(int id);
+        Task<PatientDto> CreatePatientAsync(PatientCreateDto patientCreateDto);
+        Task<PatientDto> UpdatePatientAsync(int id, PatientUpdateDto patientUpdateDto);
+        Task<bool> DeletePatientAsync(int id);
+    }
+}
