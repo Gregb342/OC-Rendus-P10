1ca9ae2fed51771e227c68c9bb1982849feafdae
Gregoire Bouteillier <gregoire.bouteillier@gmail.com>
2025-11-26 17:06:35 +0100

    Creations des settings pour lier à mongodb, ajout de mongodb et notes-backend dans le docker-compose.yml

---
+++ src/notes-backend/Infrastructure/Settings/MongoDbSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace notes_backend.Infrastructure.Settings
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string DatabaseName { get; set;} = string.Empty;
+        public string NotesCollectionName { get; set;} = string.Empty;
+    }
+}
