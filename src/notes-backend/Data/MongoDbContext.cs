b8095e2b944a2ca647e34a5210aece4f6ff42399
Gregoire Bouteillier <gregoire.bouteillier@gmail.com>
2025-11-26 17:36:23 +0100

    Ajout du mongodbcontext

---
+++ src/notes-backend/Data/MongoDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using notes_backend.Domain.Entities;
+using notes_backend.Infrastructure.Settings;
+
+namespace notes_backend.Data
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(IOptions<MongoDbSettings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            _database = client.GetDatabase(settings.Value.DatabaseName);
+        }
+
+        public IMongoCollection<Note> Notes =>
+            _database.GetCollection<Note>(GetCollectionName());
+
+        private string GetCollectionName()
+        {
+            var settings = _database.Client.Settings;
+            return "Notes";
+        }
+    
+    }
+}
