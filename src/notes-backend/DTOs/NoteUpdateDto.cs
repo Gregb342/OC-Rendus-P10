62831c166b23c9c808b7bd14c506921b4f653c54
Gregoire Bouteillier <gregoire.bouteillier@gmail.com>
2025-11-27 09:20:40 +0100

    Création des dtos

---
+++ src/notes-backend/DTOs/NoteUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace notes_backend.DTOs
+{
+    public class NoteUpdateDto
+    {
+        [Required(ErrorMessage ="Le nom du médecin est requis")]
+        public string DoctorName { get; set; } = string.Empty;
+        [Required(ErrorMessage ="Le contenu de la note est requis")]
+        public string NoteContent { get; set; } = string.Empty;
+    }
+}
