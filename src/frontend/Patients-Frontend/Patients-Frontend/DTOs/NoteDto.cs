2ef067d95200b0bc1d4118d0d4dc9154c39021e8
Gregoire Bouteillier <gregoire.bouteillier@gmail.com>
2025-12-04 16:36:00 +0100

    Affichage des notes fonctionnelles sur le frontend patient

--- src/frontend/Patients-Frontend/Patients-Frontend/DTOs/NoteDto.cs
+++ src/frontend/Patients-Frontend/Patients-Frontend/DTOs/NoteDto.cs
@@ -2,7 +2,7 @@
 {
     public class NoteDto
     {
-        public int Id { get; set; }
+        public string Id { get; set; } = string.Empty;
         public int PatientId { get; set; }
         public string DoctorName { get; set; } = string.Empty;
         public string NoteContent { get; set; } = string.Empty;
